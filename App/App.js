311ee8adab4a1773950986f89e07d65f75deacf1
reberth <reberthkss@outlook.com>
2020-03-19 17:52:09 -0300

    add Domine Font to use in the Header of the HomeScreen

--- App/App.js
+++ App/App.js
@@ -3,15 +3,31 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './Components/HomeScreen'
+
 
 const Stack = createStackNavigator();
+
+const opt = {
+  headerStyle: {backgroundColor: 'rgba(123,137,151,100)'},
+  title: 'Covid-19 Monitor',
+  headerTintColor: 'white',
+  headerTitleAlign: 'center',
+  headerTitleStyle: {fontFamily: 'Domine'},
+};
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello world!</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name={'Home'}
+            component={HomeScreen}
+            options={opt}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
