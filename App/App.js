ddbc5f26380a31461e80082f10f9796ab13f4da0
reberth <reberthkss@outlook.com>
2020-03-19 18:53:37 -0300

    Add font-vector-icons  to the project

--- App/App.js
+++ App/App.js
@@ -1,20 +1,27 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text,TouchableHighlight,TouchableOpacity} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './Components/HomeScreen'
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createStackNavigator();
 
+const searchIcon = <TouchableOpacity>
+  <View style={{padding: 10, margin: 10}}>
+    <Icon name={'search'} style={{color: 'white'}} size={15} />
+  </View>
+</TouchableOpacity>
 const opt = {
   headerStyle: {backgroundColor: 'rgba(123,137,151,100)'},
   title: 'Covid-19 Monitor',
   headerTintColor: 'white',
   headerTitleAlign: 'center',
   headerTitleStyle: {fontFamily: 'Domine'},
+  headerRight: () => searchIcon,
 };
+
 
 export default class App extends Component {
   render() {
@@ -25,6 +32,7 @@ export default class App extends Component {
             name={'Home'}
             component={HomeScreen}
             options={opt}
+
           />
         </Stack.Navigator>
       </NavigationContainer>
