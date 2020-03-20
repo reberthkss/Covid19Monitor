25ce4464c09a33ac5e331ada8b8282970d8ffa2d
reberth <reberthkss@outlook.com>
2020-03-20 18:45:24 -0300

    added method parseFlagUrlToObjectList

--- App/Providers/CountryProviders.js
+++ App/Providers/CountryProviders.js
@@ -1,6 +1,20 @@
+import CountryService from '../Service/CountryService';
 export default class CountryProviders {
+
+  static parseFlagUrlToObjectList = async (country) => {
+    let flagUrl = await CountryService.getFlagUrl(country.name);
+    let countryObj = {id:country.name,country:country.name,flagUrl:flagUrl};
+    return countryObj;
+  };
   static async getCountries() {
-    //TODO get from service
-    return [{id: '1', country: 'Brazil', flagUrl: ''}];
+    let i;
+    let countryList = [];
+    let countries = await CountryService.getCountries();
+
+    for( i = 0 ; i < 5 ; i++){
+      let countryObj = await this.parseFlagUrlToObjectList(countries[i]);
+      countryList.push(countryObj);
+    }
+    console.log(countryList)
   }
 }
