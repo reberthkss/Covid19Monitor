f6b3c33351f8bc2c8f8e666eb6de4ec8e9fa97fa
reberth <reberthkss@outlook.com>
2020-03-20 14:36:34 -0300

    refactored variable opt to headerOptionsStkScreen

--- App/App.js
+++ App/App.js
@@ -8,12 +8,13 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createStackNavigator();
 
-const searchIcon = <TouchableOpacity>
-  <View style={{padding: 10, margin: 10}}>
-    <Icon name={'search'} style={{color: 'white'}} size={15} />
-  </View>
-</TouchableOpacity>
-const opt = {
+const searchIcon = (<TouchableOpacity>
+    <View style={{padding: 10, margin: 10}}>
+      <Icon name={'search'} style={{color: 'white'}} size={15} />
+    </View>
+  </TouchableOpacity>
+);
+const headerOptionsStkScreen = {
   headerStyle: {backgroundColor: 'rgba(123,137,151,100)'},
   title: 'Covid-19 Monitor',
   headerTintColor: 'white',
@@ -31,7 +32,7 @@ export default class App extends Component {
           <Stack.Screen
             name={'Home'}
             component={HomeScreen}
-            options={opt}
+            options={headerOptionsStkScreen}
 
           />
         </Stack.Navigator>
