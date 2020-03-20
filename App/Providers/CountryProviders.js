cd1d617c46f355fe17a605bdd723d54a063e0b69
reberth <reberthkss@outlook.com>
2020-03-20 19:04:27 -0300

    explanation about usigin 5 as point-stop in for loop

--- App/Providers/CountryProviders.js
+++ App/Providers/CountryProviders.js
@@ -10,7 +10,7 @@ export default class CountryProviders {
     let i;
     let countryList = [];
     let countries = await CountryService.getCountries();
-
+// USING 5 as point-stop to don't reaches the daily request in google custom search, in the future change 5 to countries.length
     for( i = 0 ; i < 5 ; i++){
       let countryObj = await this.parseFlagUrlToObjectList(countries[i]);
       countryList.push(countryObj);
