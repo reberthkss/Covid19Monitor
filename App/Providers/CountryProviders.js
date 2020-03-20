6b9dca6a70c80e84dd7e24da384e460e426031d6
reberth <reberthkss@outlook.com>
2020-03-20 19:56:42 -0300

    removed methods about search flagUrl

--- App/Providers/CountryProviders.js
+++ App/Providers/CountryProviders.js
@@ -1,20 +1,15 @@
 import CountryService from '../Service/CountryService';
+import ImageProvider from './ImageProvider';
 export default class CountryProviders {
-
-  static parseFlagUrlToObjectList = async (country) => {
-    let flagUrl = await CountryService.getFlagUrl(country.name);
-    let countryObj = {id:country.name,country:country.name,flagUrl:flagUrl};
-    return countryObj;
-  };
   static async getCountriesList() {
     let i;
     let countryList = [];
     let countries = await CountryService.getCountries();
-// USING 5 as point-stop to don't reaches the daily request in google custom search, in the future change 5 to countries.length
+    // USING 5 as point-stop to don't reaches the daily request in google custom search, in the future change 5 to countries.length
     for( i = 0 ; i < 5 ; i++){
-      let countryObj = await this.parseFlagUrlToObjectList(countries[i]);
+      let countryObj = await ImageProvider.parseFlagUrlToObjectList(countries[i]);
       countryList.push(countryObj);
     }
-    return countryList
+    return countryList;
   }
 }
--- App/Service/CountryService.js
+++ App/Service/CountryService.js
@@ -12,11 +12,4 @@ export default class CountryService {
     let countriesJson = await countries.json();
     return countriesJson.countries;
   };
-  static getFlagUrl = async (countryName) => {
-
-    let url = `https://www.googleapis.com/customsearch/v1?searchType=image&key=AIzaSyApLRuGf4baqDYH5_qH2HQ15w_zuBv04j4&cx=012512882471033741388:2jed74gt7gl&q=flag ${countryName}`;
-    let res = await fetch(url);
-    let images = await res.json();
-    return images.items[0].image.thumbnailLink
-  };
 }
