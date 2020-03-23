98c3220510a92c6aef44a312ef1ac8f89ffd1437
reberth <reberthkss@outlook.com>
2020-03-23 14:25:33 -0300

    added a condition to return a image url of 404 Error where the RestCountries API don't found the Country name

--- App/Service/ImageService.js
+++ App/Service/ImageService.js
@@ -3,7 +3,7 @@ export default class ImageService {
     let url = `https://restcountries.eu/rest/v2/name/${countryName}`;
     let res = await fetch(url);
     let images = await res.json();
-    console.log(images.items);
-    return images.items[0].image.thumbnailLink;
+    if(images.status === 404) return "https://storage.googleapis.com/shopbr/404.png"
+    return `https://www.countryflags.io/${images[0].alpha2Code}/flat/64.png`
   };
 }
