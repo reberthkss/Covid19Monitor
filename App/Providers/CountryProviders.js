ab89b597ead2222bb0ef82cc4152cb00b1d3361e
reberth <reberthkss@outlook.com>
2020-03-23 14:28:08 -0300

    increase the number of samples

--- App/Providers/CountryProviders.js
+++ App/Providers/CountryProviders.js
@@ -6,7 +6,7 @@ export default class CountryProviders {
     let countryList = [];
     let countries = await CountryService.getCountries();
     // USING 5 as point-stop to don't reaches the daily request in google custom search, in the future change 5 to countries.length
-    for( i = 0 ; i < 5 ; i++){
+    for( i = 0 ; i < 10 ; i++){
       let countryObj = await ImageProvider.parseFlagUrlToObjectList(countries[i]);
       countryList.push(countryObj);
     }
