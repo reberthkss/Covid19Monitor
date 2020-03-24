f08b7bb040f5a041fe9609d83b88d28cfcfafb09
reberth <reberthkss@outlook.com>
2020-03-24 14:40:37 -0300

    added styles directory with colors and general styles

---
+++ App/styles/colors.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const colors = {
+  primary: '#5E92C0',
+  secondary: '#5A6978',
+  transparent: 'rgba(0,0,0,0)',
+  itemList: 'black',
+  subtitleNum: 'black',
+  subtitleProfile: '#C0CCDA',
+};
+
+export default colors;
---
+++ App/styles/general.js
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native';
+
+const geralStyles = StyleSheet.create({
+  loading: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+});
+
+export default geralStyles;
