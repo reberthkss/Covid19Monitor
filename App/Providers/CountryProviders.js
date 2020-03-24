29b1cc2c049080f2a6208dea331a0ba7dcb7ce16
reberth <reberthkss@outlook.com>
2020-03-24 14:37:48 -0300

    changed by 10 to countries.lenght to get full list of couuntries

--- App/Providers/CountryProviders.js
+++ App/Providers/CountryProviders.js
@@ -5,9 +5,11 @@ export default class CountryProviders {
     let i;
     let countryList = [];
     let countries = await CountryService.getCountries();
-    // USING 5 as point-stop to don't reaches the daily request in google custom search, in the future change 5 to countries.length
-    for( i = 0 ; i < 10 ; i++){
-      let countryObj = await ImageProvider.parseFlagUrlToObjectList(countries[i]);
+
+    for (i = 0; i < countries.length; i++) {
+      let countryObj = await ImageProvider.parseFlagUrlToObjectList(
+        countries[i],
+      );
       countryList.push(countryObj);
     }
     return countryList;
