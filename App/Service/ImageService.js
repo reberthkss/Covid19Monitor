ac6d2d5303e952a06874465dc9b3d981bb10d727
reberth <reberthkss@outlook.com>
2020-03-25 11:57:35 -0300

    condition changed when status code of request is 404

--- App/Service/ImageService.js
+++ App/Service/ImageService.js
@@ -2,8 +2,8 @@ export default class ImageService {
   static getFlagUrl = async countryName => {
     let url = `https://restcountries.eu/rest/v2/name/${countryName}`;
     let res = await fetch(url);
+    if (!res.ok) return 'https://storage.googleapis.com/shopbr/404.png';
     let images = await res.json();
-    if(images.status === 404) return "https://storage.googleapis.com/shopbr/404.png"
-    return `https://www.countryflags.io/${images[0].alpha2Code}/flat/64.png`
+    return `https://www.countryflags.io/${images[0].alpha2Code}/flat/64.png`;
   };
 }
